ee16329d7b671ae88b82ccef09858dcae35c32e6
Rubén Álvarez <info@rubenalvarez.dev>
2024-05-27 00:39:19 +0200

    half shift

--- src/components/User/UserForm.jsx
+++ src/components/User/UserForm.jsx
@@ -1,44 +1,7 @@
-import { useState } from 'react';
-import { createUser } from '../../services/api';
+import React from 'react'
 
 export const UserForm = () => {
-  const [formData, setFormData] = useState({
-    name: '',
-    first_surname: '',
-    second_surname: '',
-    email: '',
-    phone: '',
-    password: '',
-  });
-
-  const handleChange = (e) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      await createUser(formData);
-      setFormData({
-        name: '',
-        first_surname: '',
-        second_surname: '',
-        email: '',
-        phone: '',
-        password: '',
-      });
-    } catch (error) {
-      console.error('Error creating user:', error);
-    }
-  };
-
   return (
-    <div className="user-form card">
-      <h2>Create User</h2>
-      <form onSubmit={handleSubmit}>
-        {/* Render form fields */}
-        <button type="submit">Create</button>
-      </form>
-    </div>
-  );
-};
+    <div>UserForm</div>
+  )
+}
